5795a3b8698345cdfdfb7a38aec99ce4ca5ef45a
LakshmiPrashanth2025 <lakshmi.prashant@gmail.com>
2026-06-14 16:14:23 +0530

    Add files via upload

---
+++ src/test/java/com/example/spring/SecurityApp/tests/SecurityAppApplicationTests.java
@@ -0,0 +1,32 @@
+package com.example.spring.SecurityApp.tests;
+
+import com.example.spring.SecurityApp.entities.User;
+import com.example.spring.SecurityApp.services.JwtService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityAppApplicationTests {
+
+	@Autowired
+	private JwtService jwtService;
+
+	@Test
+	void contextLoads() {
+
+		User user = new User(4L, "test@gmail.com", "1234", "TestUser");
+
+		String token = jwtService.generateAccessToken(user);
+
+		System.out.println(token);
+
+		Long id = jwtService.getUserIdFromToken("eyJhbGciOiJIUzM4NCJ9" +
+				".eyJzdWIiOiI0IiwiZW1haWwiOiJhbnVqQGdtYWlsLmNvbSIsInJvbGVzIjpbIlVTRVIiLCJBRE1JTiJdLCJpYXQiOjE3MjE2NTMyNTgsImV4cCI6MTcyMTY1MzMxOH0.fhjAtKv56iN9XsyPxNh6V3rr6Eds9LuWVkqklcRQNzgTq7GY-46TG2LFvyDshiZe");
+
+		System.out.println(id);
+
+	}
+
+}
